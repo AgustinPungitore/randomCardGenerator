02389b2e908fae42b93c192c49f4d94e109d3ccb
AgustinPungitore <agustinpungitore@gmail.com>
2022-08-13 03:12:29 +0000

    logic part finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+let palos = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let random1 = Math.floor(Math.random() * cards.length);
+  let random2 = Math.floor(Math.random() * palos.length);
+
+  document.write(cards[random1] + " " + palos[random2]);
 };
+
+document.querySelector();
